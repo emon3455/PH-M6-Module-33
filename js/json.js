69bc81201e8746aa4f550f72327b1868bc932764
emon3455 <emon.mhk69@gmail.com>
2023-02-23 00:37:01 +0600

    json conversion done

---
+++ js/json.js
@@ -0,0 +1,20 @@
+const student ={
+    name: "Emon",
+    address:{
+        area:"Mohakhali",
+        city:"Dhaka",
+        Country:"Bangladesh",
+    },
+    cgpa:3.82,
+    isSingle: true,
+    isHappy: false,
+    dreams: ["To Be a Software Enginner", "To be a good husband","To be a good human bing"],
+}
+
+// for converting js variable or object in json:
+const studentJson = JSON.stringify(student);
+console.log(studentJson);
+
+// for converting JSON Into js element:
+const newStudent = JSON.parse(studentJson);
+console.log(newStudent);
